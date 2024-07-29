37e8f6caf19a3b054640d7ae85eea447cdb1b902
Valerii <valeraviachalo@gmail.com>
2024-07-29 13:10:58 +0300

    m

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const config = {
-  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001",
-};
-
-export default config;
--- src/components/Layout/FetchData/FetchData.js
+++ src/components/Layout/FetchData/FetchData.js
@@ -1,7 +1,5 @@
-import config from "../../../../config"; // імпорт конфігурації
-
 export const fetchData = async (file) => {
-  const response = await fetch(`${config.BASE_URL}${file}`);
+  const response = await fetch(`https://seo-exp.vercel.app/${file}`);
   const data = await response.json();
   return data;
 };
